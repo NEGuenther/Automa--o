b4af614e27d24aa37a9e1861833d720ba987168a
Nickolas Guether <82460137+NEGuenther@users.noreply.github.com>
2026-01-07 10:50:42 -0300

    Add functionality for processing materials and norms, update internal comments insertion, and refine Excel handling

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from inserir_codigos_de_itens import gerar_planilha_com_codigos
-from inserir_internal_comment import inserir_dados
+from inserir_internal_comment import inserir_internal_coments
 from inserir_traducoes import Traducoes
 
 
@@ -13,12 +13,15 @@ gerar_planilha_com_codigos(
 print("Planilha atualizada gerada com sucesso.")
 
 # 2) Insere dados (narrativa SAP123, etc.)
-inserir_dados()
+inserir_internal_coments(
+	caminho_planilha_atualizada=r"planilhas/planilha_atualizada.xlsx",
+	caminho_base_totvs=r"planilhas/base_dados_TOTVS.xlsx",
+)
 
 # 3) Usa a classe Traducoes, referenciando a coluna "SAP123" como base
 trad = Traducoes(
 	caminho_narrativas=r"planilhas/planilha_atualizada.xlsx",   # deve ter coluna 'SAP123'
-	caminho_traducoes=r"DADOS/dicionario.xlsx",       # deve ter coluna base em português + colunas de idiomas
+	caminho_traducoes=r"dados/dicionario.xlsx",       # deve ter coluna base em português + colunas de idiomas
 	coluna_narrativa="SAP123",
 	coluna_portugues="MAKTX(PT)",  # nome real da coluna em português na planilha de dicionário
 )
